d99fb41035a5d9b6f19fe017bb6516a12362a724
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-07 01:59:33 -0500

    Color de fondo negro- color de texto blanco y favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
     title: "Tareas",
     description: "CRUD de tareas",
+    icons: {
+        icon: [
+            { url: "/next.svg", href: "/next.svg" },
+        ],
+    },
 };
-
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
