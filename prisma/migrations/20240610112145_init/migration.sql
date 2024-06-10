44cb630b0bace7c9034bd9078634b924f95597b5
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-10 06:26:34 -0500

    Implementando Prisma

---
+++ prisma/migrations/20240610112145_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "titulo" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
