7f2ab1c7d1cd83920f59c01539b6b86c94a7871a
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-11 07:35:09 -0500

    Obteniendo task con findUnique

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,9 +1,14 @@
+import { prisma } from "@/app/libs/prisma";
 import { NextResponse } from "next/server";
 
-export function GET(request, { params }) {
-    return NextResponse.json({
-        message: `Obteniendo Tarea ${params.id}`,
+export async function GET(request, { params }) {
+    const task = await prisma.task.findUnique({
+        where: {
+            id: Number(params.id),
+        },
     });
+    console.log(task);
+    return NextResponse.json(task);
 }
 export function PUT(request, { params }) {
     return NextResponse.json({
