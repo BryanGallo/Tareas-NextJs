fed8bca66cdaf494ce9cf671a0efc0e9ded931e2
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-11 23:24:33 -0500

    Editando task con update

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -1,4 +1,5 @@
 import { prisma } from "@/app/libs/prisma";
+import { taintUniqueValue } from "next/dist/server/app-render/rsc/taint";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
@@ -10,11 +11,25 @@ export async function GET(request, { params }) {
     console.log(task);
     return NextResponse.json(task);
 }
-export function PUT(request, { params }) {
-    return NextResponse.json({
-        message: `Editando Tarea ${params.id}`,
+
+export async function PUT(request, { params }) {
+    // const { title, description } = await request.json();
+    const data = await request.json();
+    console.log(data);
+    const taskUpdate = await prisma.task.update({
+        where: {
+            id: Number(params.id),
+        },
+        data: data,
+        // data: {
+        //     // Cuando queremos actualizar parcialmente no extraemos y mandamos el objeto
+        //     titulo: title,
+        //     description: description,
+        // },
     });
+    return NextResponse.json(taskUpdate);
 }
+
 export async function DELETE(request, { params }) {
     try {
         const task = await prisma.task.findUnique({
