b489173ed21e7d0bd84917f9e395ab57026dedb8
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-11 23:07:39 -0500

    Borrando task con delete

--- src/app/api/tasks/[id]/route.js
+++ src/app/api/tasks/[id]/route.js
@@ -15,8 +15,30 @@ export function PUT(request, { params }) {
         message: `Editando Tarea ${params.id}`,
     });
 }
-export function DELETE(request, { params }) {
-    return NextResponse.json({
-        message: `Eliminando Tarea ${params.id}`,
-    });
+export async function DELETE(request, { params }) {
+    try {
+        const task = await prisma.task.findUnique({
+            where: {
+                id: Number(params.id),
+            },
+        });
+        if (!task) {
+            return NextResponse.json({
+                message: `No hay nada aqui`,
+            });
+        }
+        await prisma.task.delete({
+            where: {
+                id: Number(params.id),
+            },
+        });
+
+        return NextResponse.json({
+            message: `Eliminando Tarea ${params.id}`,
+        });
+    } catch (error) {
+        return NextResponse.json({
+            message: error.message,
+        });
+    }
 }
