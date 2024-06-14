803dcdc0ae8629bd1ee00749c3cf651915e56111
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-14 06:40:53 -0500

    Formulario New Tarea

--- src/app/tasks/new/page.jsx
+++ src/app/tasks/new/page.jsx
@@ -1,7 +1,35 @@
 const NewTask = () => {
     return (
-        <div>
-            <h2 className="text-2xl">Formulario nueva tarea</h2>
+        <div className="container h-screen flex justify-center mx-auto items-center">
+            <form className="bg-slate-700 p-10 text-gray-950 w-2/4">
+                <h2 className="text-3xl text-center mb-4 font-bold text-white ">
+                    Add Task
+                </h2>
+                <label htmlFor="title" className="text-white font-bold">
+                    Titulo
+                </label>
+                <input
+                    id="title"
+                    type="text"
+                    placeholder="Titulo Tarea"
+                    className=" w-full mb-4 p-2 rounded-lg"
+                />
+                <label htmlFor="description" className="text-white font-bold">
+                    Descripcion
+                </label>
+                <textarea
+                    id="description"
+                    placeholder="Descripcion"
+                    rows="3"
+                    className="p-2 w-full mx-auto rounded-lg"
+                />
+                <button
+                    type="submit"
+                    className="p-2 rounded-lg bg-white hover:bg-blue-400 hover:text-white mt-2"
+                >
+                    Add Task
+                </button>
+            </form>
         </div>
     );
 };
