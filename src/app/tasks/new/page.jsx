b64f11c19e3eaeb297b799ee3e540a56f484324f
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-14 07:17:29 -0500

    Funcion handleSubmit conectado /api/tasks crea Tarea

--- src/app/tasks/new/page.jsx
+++ src/app/tasks/new/page.jsx
@@ -1,7 +1,33 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const NewTask = () => {
+    const router = useRouter();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log("Enviar Formulario");
+        const title = e.target.title.value;
+        const description = e.target.description.value;
+
+        const res = await fetch("/api/tasks", {
+            method: "POST",
+            body: JSON.stringify({ title, description }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        const data = await res.json();
+        console.log(data);
+        router.push(`/`);
+    };
+
     return (
         <div className="container h-screen flex justify-center mx-auto items-center">
-            <form className="bg-slate-700 p-10 text-gray-950 w-2/4">
+            <form
+                onSubmit={handleSubmit}
+                className="bg-slate-700 p-10 text-gray-950 w-2/4"
+            >
                 <h2 className="text-3xl text-center mb-4 font-bold text-white ">
                     Add Task
                 </h2>
@@ -13,6 +39,7 @@ const NewTask = () => {
                     type="text"
                     placeholder="Titulo Tarea"
                     className=" w-full mb-4 p-2 rounded-lg"
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label htmlFor="description" className="text-white font-bold">
                     Descripcion
@@ -22,12 +49,13 @@ const NewTask = () => {
                     placeholder="Descripcion"
                     rows="3"
                     className="p-2 w-full mx-auto rounded-lg"
+                    onChange={(e) => setDescription(e.target.value)}
                 />
                 <button
                     type="submit"
                     className="p-2 rounded-lg bg-white hover:bg-blue-400 hover:text-white mt-2"
                 >
-                    Add Task
+                    Crear Tarea
                 </button>
             </form>
         </div>
