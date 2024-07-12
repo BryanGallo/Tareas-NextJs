9ef7aee5ad8b380256b0bc3fbfe6481baec42b30
Bryan Gallo <bryangallo95@hotmail.com>
2024-07-11 23:01:47 -0500

    Cambiando titulo About

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-    return <p className="text-center text-black text-4xl">About Tasks</p>;
+    return <p className="text-center text-black text-4xl">CRUD DE TAREAS CON NEXT JS</p>;
 };
 
 export default About;
